cc2c1f2329a5c1cbf869079680d20cfc83d335d2
Dillan <lan1menas@gmail.com>
2024-10-01 18:16:41 -0600

    Constructor de la clase cuenta y accesesores

--- Proyecto1-POO/src/main/java/com/mycompany/proyecto1/poo/logicadenegocios/Cuenta.java
+++ Proyecto1-POO/src/main/java/com/mycompany/proyecto1/poo/logicadenegocios/Cuenta.java
@@ -6,8 +6,30 @@ package com.mycompany.proyecto1.poo.logicadenegocios;
 
 /**
  *
- * @author Dillan
+ * @author Dillan, Maikel, Tayler
  */
 public class Cuenta {
+  private final int numCuenta;
+  private final String estatus;
+  private final int saldo;
+  private final int idDueno;
+  
+    /**
+   * Método constructor de la clase cuenta
+   *
+   * @param pNumCuenta
+   * @param pEstatus
+   * @param pSaldo
+   * @param pIdDueno
+   */
+  public Cuenta(int pIdDueno,int pNumCuenta,String pEstatus,int pSaldo){
+    idDueno = pIdDueno;
+    numCuenta = pNumCuenta;
+    estatus = pEstatus;
+    saldo = pSaldo;
+  }
+    public int getIdDueno() {
+    return idDueno;
+  }
     
 }
