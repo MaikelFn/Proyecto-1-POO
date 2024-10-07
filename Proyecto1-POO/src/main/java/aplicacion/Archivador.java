c2d6006fcf338f232d11434a10a1b2c7164b8ae8
Dillan <lan1menas@gmail.com>
2024-10-07 12:38:56 -0600

    Creacion de la clase que maneja archivos xml

--- Proyecto1-POO/src/main/java/aplicacion/Archivador.java
+++ Proyecto1-POO/src/main/java/aplicacion/Archivador.java
@@ -3,11 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package aplicacion;
+import java.util.LinkedList;
 
 /**
  *
  * @author Dillan
  */
 public class Archivador {
-    
+  private final String tipoDato;
+  private final LinkedList<String> datos;
+  
+  public Archivador (){
+    tipoDato = "";
+    datos = new LinkedList<>();
+  }
 }
